3490f52b2c9dd3b161a39e2fdb4306e107597b1b
BaphomeT-T <pdavila0113@gmail.com>
2023-11-17 23:14:31 -0500

    Primera Version

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int numTerminos;
+        
+        System.out.println("Grupo: ....");
+        System.out.println("Integrantes: Julian Camacho, Juan Chanatasig, Andrea Chicaiza, Antony Cobos, Paul Davila, Edison Dueñas, Isabella Hernandez" );
+
+
+    }
+}
