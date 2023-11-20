cb400ee2ec9a1dde12aa2acd2195500516124436
BaphomeT-T <pdavila0113@gmail.com>
2023-11-20 18:40:57 -0500

    reto

--- src/App.java
+++ src/App.java
@@ -8,7 +8,7 @@ public class App {
         int numTerminos;
         
         System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
-        System.out.println("Grupo: ...");
+        System.out.println("Grupo: ....");
         System.out.println("Integrantes: Julian Camacho, Juan Chanatasig, Andrea Chicaiza, Antony Cobos, Paul Davila, Edison Dueñas, Isabella Hernandez" );
         System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
 
