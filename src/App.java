229ca545378f5efd27782e0a30d12876e90a1869
lalabell-a <150473705+lalabell-a@users.noreply.github.com>
2023-11-20 23:20:21 -0500

    Update App.java

--- src/App.java
+++ src/App.java
@@ -5,6 +5,8 @@ public class App {
         Scanner sc = new Scanner(System.in);
         SerieNumerica10 oSerieNumerica10 = new SerieNumerica10();
         Ejercicio5 oEjercicio5 = new Ejercicio5();
+        SerieNumerica8 oSerieNumerica8 = new SerieNumerica8();
+        SerieNumerica9 oSerieNumerica9 = new SerieNumerica9();
 
         int numTerminos;
         
@@ -36,6 +38,32 @@ public class App {
         System.out.println("Usando doWhile:");
         oEjercicio5.showSerie5doWhile(numTerminos);
         System.out.println("");
+
+        System.out.println("Serie 8: 3, 8, 13, 18, 23, 28, 33, 38, ...");
+        System.out.println("....................................");
+        System.out.println("Método for:");
+        oSerieNumerica8.showserie8For(numTerminos);
+        System.out.println(".....................................");
+        System.out.println("Método while:");
+        oSerieNumerica8.showserie8while(numTerminos);
+        System.out.println(".....................................");
+        System.out.println("Método Do-while:");
+        oSerieNumerica8.showserie8dowhile(numTerminos);
+        System.out.println(".....................................");
+
+
+        System.out.println("Serie 9: 2, 4, 8, 16, 32, 64, ...");
+        System.out.println("....................................");
+        System.out.println("Método for:");
+        oSerieNumerica9.showserie9For(numTerminos);
+        System.out.println(".....................................");
+        System.out.println("Método while:");
+        oSerieNumerica9.showserie9while(numTerminos);
+        System.out.println(".....................................");
+        System.out.println("Método Do-while:");
+        oSerieNumerica9.showserie9dowhile(numTerminos);
+        System.out.println(".....................................");
+
 
         System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
         System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
