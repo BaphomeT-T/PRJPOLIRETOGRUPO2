6a044609d3bdc6f3dc2ea1da53c38d772225cb30
Juan <jfchanatasig1405@gmail.com>
2023-11-20 23:16:16 -0500

    Descripción de los cambios en el submodule

--- src/App.java
+++ src/App.java
@@ -4,19 +4,58 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         SerieNumerica10 oSerieNumerica10 = new SerieNumerica10();
+        Ejercicio5 oEjercicio5 = new Ejercicio5();
         SerieNumerica03 oSerieNumerica03 = new SerieNumerica03();
 
         int numTerminos;
         
+        System.out.println("---------------------------------------------------------------------------------------------------------------------------------------------");
         System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
-        System.out.println("Grupo: ....");
+        System.out.println("");
+        System.out.println("Grupo: ...");
         System.out.println("Integrantes: Julian Camacho, Juan Chanatasig, Andrea Chicaiza, Antony Cobos, Paul Davila, Edison Dueñas, Isabella Hernandez" );
+         System.out.println("");
         System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
+      
 
-
+         System.out.println("");
         System.out.println("Ingrese el número de veces que desea repetir las series");
         numTerminos = Integer.parseInt(sc.nextLine());
+         System.out.println("");
+
         System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
+        
+        System.out.println("");
+        System.out.println("Serie 3: 0/1  1/3  1/5  2/7  3/9   5/11  8/13  13/15 ... ");
+        System.out.println("Usando For");
+        oSerieNumerica03.showSerie03For(numTerminos);
+        System.out.println("");
+        System.out.println("Usando el While");
+        oSerieNumerica03.showSerie03While(numTerminos);
+        System.out.println("");
+        System.out.println("Usando el Do While");
+        oSerieNumerica03.showSerie03DoWhile(numTerminos);
+        System.out.println("");
+
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("");
+        System.out.println("Serie 5: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, ...");
+        System.out.println("Usando for:");
+        oEjercicio5.showSerie5For(numTerminos);
+        System.out.println("");
+        System.out.println("Usando While:");
+        oEjercicio5.showSerie5While(numTerminos);
+        System.out.println("");
+        System.out.println("Usando doWhile:");
+        oEjercicio5.showSerie5doWhile(numTerminos);
+        System.out.println("");
+
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("");
 
         System.out.println("Serie 10: 3, 9, 27, 81, 243, 729, 2187, ...");
         System.out.println("Usando for:");
@@ -27,10 +66,10 @@ public class App {
         oSerieNumerica10.showSerie10doWhile(numTerminos);
         System.out.println("Usando Recursividad:");
         oSerieNumerica10.showSerie10Recursividad(numTerminos,3);
+
+        System.out.println("");
         System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
-       
-        System.out.println("Serie 03: 0/1  1/3  1/5  2/7  3/9   5/11  8/13  13/15 ...");
-        System.out.println("Con el Ciclo for:");
-        oSerieNumerica03.showSerie03For(numTerminos);
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
+
     }
 }
--- src/SerieNumerica03.java
+++ src/SerieNumerica03.java
@@ -12,8 +12,39 @@ public class SerieNumerica03{
             System.out.print(numerador + "/" + denominador + " ");
     }
 }
+/**
+ * b>showSerie03While</b> muestra la serie: 0/1  1/3  1/5  2/7  3/9   5/11  8/13  13/15 ... usando While.
+ * @param numTerminos
+ */
+    public void showSerie03While(int numTerminos){
+        int i=0;
+        while (i<numTerminos) {
+            int numerador=serieFibonacii(i);
+            int denominador=2*i+1;
+            System.out.print(numerador +"/"+ denominador+" ");
+            i++;
+        }
+    }
+/**
+ * b>showSerie03DoWhile</b> muestra la serie: 0/1  1/3  1/5  2/7  3/9   5/11  8/13  13/15 ... usando DoWhile.
+ * @param numTerminos
+ */
+    public void showSerie03DoWhile(int numTerminos){
+         int i=0;
+        do {
+            int numerador=serieFibonacii(i);
+            int denominador=2*i+1;
+            System.out.print(numerador+"/"+denominador+" ");
+            i++;
+        } while (i<numTerminos);
 
-   
+    }
+
+   /**
+    * b>showSerie03While</b> muestra la serie fibonacci.
+    * @param numTerminos
+    * @return
+    */
     private int serieFibonacii(int numTerminos ){
         
         return (numTerminos<=1)? numTerminos:serieFibonacii(numTerminos-1)+serieFibonacii(numTerminos-2); 
