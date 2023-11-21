356398760f6267ad297fd0af5e4b50f8cb626dd7
andrea-m11 <andreachicaiza11@gmail.com>
2023-11-21 00:30:40 -0500

    Series 6 y 7

--- src/App.java
+++ src/App.java
@@ -6,8 +6,6 @@ public class App {
         Ejercicio5 oEjercicio5 = new Ejercicio5();
         SerieNumerica6 oSerieNumerica6 = new SerieNumerica6();
         SerieNumerica7 oSerieNumerica7 = new SerieNumerica7();
-        SerieNumerica8 oSerieNumerica8 = new SerieNumerica8();
-        SerieNumerica9 oSerieNumerica9 = new SerieNumerica9();
         SerieNumerica10 oSerieNumerica10 = new SerieNumerica10();
 
         int numTerminos;
@@ -70,37 +68,6 @@ public class App {
         System.out.println("Usando Recursividad:");
         oSerieNumerica7.showSerie7Recursividad(1,numTerminos);
         System.out.println("");
-
-        System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
-        System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
-        System.out.println("");
-      
-        System.out.println("Serie 8: 3, 8, 13, 18, 23, 28, 33, 38, ...");
-        System.out.println("Método for:");
-        oSerieNumerica8.showserie8For(numTerminos);
-        System.out.println("");
-        System.out.println("Método while:");
-        oSerieNumerica8.showserie8while(numTerminos);
-       System.out.println("");
-        System.out.println("Método Do-while:");
-        oSerieNumerica8.showserie8dowhile(numTerminos);
-        System.out.println("");
-
-        System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
-        System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
-        System.out.println("");
-      
-        System.out.println("Serie 9: 2, 4, 8, 16, 32, 64, ...");
-       System.out.println("");
-        System.out.println("Método for:");
-        oSerieNumerica9.showserie9For(numTerminos);
-        System.out.println("");
-        System.out.println("Método while:");
-        oSerieNumerica9.showserie9while(numTerminos);
-        System.out.println("");
-        System.out.println("Método Do-while:");
-        oSerieNumerica9.showserie9dowhile(numTerminos);
-       System.out.println("");
 
         System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
         System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
