9024ef1c3a57f35175fb38fdc9365e7680bb85eb
BaphomeT-T <pdavila0113@gmail.com>
2023-11-30 17:11:08 -0500

    Menu inicial

--- src/CadenaCaracteres5.java
+++ src/CadenaCaracteres5.java
@@ -4,24 +4,23 @@
  * Version 3.0
  */
 public class CadenaCaracteres5 {
-    public void showCadenaCaracteres5 (String palabraOriginal){
-      StringBuilder palabraInvertia = new StringBuilder();
-      for(int i=palabraOriginal.length() -1;i>=0 ;i--){
-        char c = palabraOriginal.charAt(i);
-        if ((c == 'a') || (c=='e')||(c=='i')||(c=='o')||(c=='u')) {
-            palabraInvertia.append(Character.toUpperCase(c));
-        } else {
-            palabraInvertia.append(c);
+    /**
+     * Invierte una frase y cambbia sus vocales por mayúsculas
+     * 
+     * @param palabraOriginal The original string to be reversed and have its vowels converted to uppercase.
+     * @return The reversed string with uppercase vowels.
+     */
+    public void showCadenaCaracteres5(String palabraOriginal) {
+        StringBuilder palabraInvertida = new StringBuilder();
+        for (int i = palabraOriginal.length() - 1; i >= 0; i--) {
+            char c = palabraOriginal.charAt(i);
+            if ((c == 'a') || (c == 'e') || (c == 'i') || (c == 'o') || (c == 'u')) {
+                palabraInvertida.append(Character.toUpperCase(c));
+            } else {
+                palabraInvertida.append(c);
+            }
         }
-      }
-      System.out.println("Palabra invertia: "+ palabraInvertia);
+        System.out.println("Palabra invertida: " + palabraInvertida);
     }
-    /*private boolean determinarVocal (char caractrerDeterminar){
-        char c = Character.toLowerCase(caractrerDeterminar);
-        if ((c == 'a') || (c=='e')||(c=='i')||(c=='o')||(c=='u')){
-            return true;
-        }else{
-            return false;
-        }
-    }*/
+   
 }
